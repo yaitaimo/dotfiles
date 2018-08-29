cdc39ef4f0a568e8c6c69681d3b52c4cb4c3628b
yaitaimo <yaitaimo@gmail.com>
2018-08-29 15:08:56 +0900

    各言語用の設定を追記

--- config.fish
+++ config.fish
@@ -1,13 +1,24 @@
 set -x EDITOR vim
 set -x LC_CTYPE "ja_JP.UTF-8"
-set -x PATH /usr/local/bin $PATH
 
-# direnv
-eval (direnv hook fish)
+if status --is-interactive
+    # direnv
+    eval (direnv hook fish)
 
-# pyenv
-status --is-interactive; and . (pyenv init -|psub)
+    # rbenv
+    . (rbenv init -|psub)
 
+    # pipenv
+    eval (pipenv --completion)
+
+    # pyenv
+    . (pyenv init -|psub)
+
+    # nodenv
+    . (nodenv init -|psub)
+end
+
+# git on prompt
 set __fish_git_prompt_showdirtystate yes
 set __fish_git_prompt_showstashstate yes
 
@@ -29,6 +40,7 @@ set __fish_git_prompt_color_invalidstate blue
 set __fish_git_prompt_color_untrackedfiles red
 set __fish_git_prompt_color_cleanstate cyan
 
+# prompt
 function fish_prompt
   set last_status $status
 
@@ -45,6 +57,7 @@ function fish_prompt
   printf '\n$ '
 end
 
+# fzf config
 set -x FZF_COMPLETE 1
 set -x FZF_DEFAULT_OPTS "--reverse --height 40%"
 set -x FZF_DEFAULT_COMMAND 'ag -g ""'
@@ -53,6 +66,7 @@ function fssh -d "Fuzzy-find ssh host and ssh into it"
   ag '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | xargs -o ssh
 end
 
+# alias
 alias v 'nvim'
 alias ls 'ls -hG'
 alias ll 'ls -lhG'
@@ -68,6 +82,7 @@ alias ga 'git add'
 alias gc 'git commit -m'
 alias gb 'git branch'
 
+# reload
 function reload
     source ~/.config/fish/config.fish
 end
