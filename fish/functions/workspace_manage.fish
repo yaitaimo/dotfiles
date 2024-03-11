d0b30573ac745e4cd8cccffe87a03d48504e48ae
yaitaimo <yaitaimo@gmail.com>
2024-03-11 12:01:06 +0900

    入力がキャンセルされた場合に対応

--- fish/functions/workspace_manage.fish
+++ fish/functions/workspace_manage.fish
@@ -1,28 +1,24 @@
 function workspace_manage -d 'Manage workspaces with search or create functionality'
     # fzf でワークスペースを選択または新規作成用の名前を入力
-    set -l results (ls ~/Workspace | fzf --print-query --height=10 --prompt="Select or type new workspace name: ")
-    set -l query (echo $results | head -n 1)
-    set -l selected_workspace (echo $results | tail -n +2)
+    set -l query (ls ~/Workspace | fzf +m --print-query | head -n 2)
 
-    # ユーザが何も選択しなかった場合の対応
-    if not [ -n "$query" ]
-        echo "No workspace selected or entered. Operation cancelled."
+    # 入力がない場合は終了
+    if test -z "$query"
+        commandline -f repaint
         return 1
     end
 
-    # ユーザが入力したクエリを使用してワークスペース名を決定
+    # ワークスペース名を決定
     set -l workspace_name (string trim "$query")
 
     # ワークスペースのフルパスを設定
     set -l workspace_path ~/Workspace/"$workspace_name"
 
-    # 選択または入力されたワークスペースが既に存在するかチェック
-    if [ -d "$workspace_path" ]
-        # 存在する場合はそのディレクトリに移動
+    # ワークスペースが存在する場合は移動、存在しない場合は作成して移動
+    if test -d "$workspace_path"
         echo "Entering workspace '$workspace_name'."
         cd "$workspace_path"
     else
-        # 存在しない場合は新しくディレクトリを作成して移動
         echo "Creating and entering new workspace: '$workspace_name'."
         mkdir -p "$workspace_path"
         cd "$workspace_path"
