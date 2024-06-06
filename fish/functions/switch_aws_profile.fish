2d7835ab936bf652457ba54a3095646e6238cf40
yaitaimo <yaitaimo@gmail.com>
2024-06-06 21:28:46 +0900

    AWS Profile 設定用の関数を追加

---
+++ fish/functions/switch_aws_profile.fish
@@ -0,0 +1,22 @@
+function switch_aws_profile
+    # ~/.aws/config からプロファイル名を抽出
+    set profiles (grep '^\[profile ' ~/.aws/config | sed -e 's/^\[profile //' -e 's/\]$//')
+
+    # プロファイルが見つからなければエラーメッセージを表示
+    if test (count $profiles) -eq 0
+        echo "No AWS profiles found in ~/.aws/config"
+        return 1
+    end
+
+    # fzf でプロファイルを選択
+    set selected_profile (echo $profiles | tr ' ' '\n' | fzf --prompt="Select an AWS profile: ")
+
+    # 選択したプロファイルを検証
+    if test -n "$selected_profile"
+        echo "Selected profile: $selected_profile"
+        set -gx AWS_PROFILE $selected_profile
+    else
+        echo "No profile selected"
+        return 1
+    end
+end
