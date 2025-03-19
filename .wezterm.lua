f09f3bd3aa1a4c86858719a92ade80bbbc7bb28e
yaitaimo <yaitaimo@gmail.com>
2025-03-19 23:14:38 +0900

    フォントを設定

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font_with_fallback({
+    { family = 'RobotoMono Nerd Font' },
+    { family = 'RobotoMono Nerd Font', assume_emoji_presentation = true },
+    { family = 'ヒラギノ角ゴシック' },
+})
 config.font_size = 17
+
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 
