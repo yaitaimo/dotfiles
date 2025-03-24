6884f63f10a8e99e946fb785a0f0cf325d1d7978
yaitaimo <yaitaimo@gmail.com>
2025-03-24 20:15:22 +0900

    不要な設定を削除

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,12 @@
--- Autocmdグループの作成
-local my_group = vim.api.nvim_create_augroup("MyAutoCmd", { clear = true })
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
