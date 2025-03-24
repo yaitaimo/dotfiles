5ede10a42bc3b90829ca9f280e7afd23149af7ee
yaitaimo <yaitaimo@gmail.com>
2025-03-24 22:10:10 +0900

    LSPの設定をシンプルに修正

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,13 +2,13 @@
 require("lazy").setup({
   spec = {
     {
-      "williamboman/mason.nvim",
-      config = true,
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+      },
       config = function()
+        require("mason").setup({})
         require("mason-lspconfig").setup({
           ensure_installed = {
             "lua_ls",   -- Lua
@@ -26,20 +26,41 @@ require("lazy").setup({
             "graphql",  -- GraphQL
           },
         })
-      end,
-    },
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
+        local on_attach = function(_, _)
+          local map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+          end
+          map("n", "gd", vim.lsp.buf.definition, "Go to Definition")
+          map("n", "gr", vim.lsp.buf.references, "Find References")
+          map("n", "gi", vim.lsp.buf.implementation, "Go to Implementation")
+          map("n", "K", vim.lsp.buf.hover, "Show Hover")
+          map("n", "<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
+          map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+          map("n", "<leader>f", function()
+            vim.lsp.buf.bormat({ async = true })
+          end, "Format")
+        end
+
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
+
+        require("mason-lspconfig").setup_handlers({
+          function(server_name)
+            lspconfig[server_name].setup({
+              on_attach = on_attach,
+            })
+          end,
+          ["lua_ls"] = function()
+            lspconfig.lua_ls.setup({
+              on_attach = on_attach,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                },
               },
-            },
-          },
+            })
+          end,
         })
       end,
     },
