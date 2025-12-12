b1dd7589ac874af9630c57180dbd47b82a6cc032
yaitaimo <yaitaimo@gmail.com>
2025-12-12 15:55:59 +0900

    git.luaにDiffviewプラグインを追加し、キーマッピングを設定

--- nvim/lua/plugins_specs/git.lua
+++ nvim/lua/plugins_specs/git.lua
@@ -1,8 +1,18 @@
--- Placeholder for Git-related plugins (future)
+-- Git-related plugins
 return {
-  -- Example to add later:
-  -- {
-  --   "lewis6991/gitsigns.nvim",
-  --   opts = {},
-  -- },
+  -- Diffview: clean UI for reviewing diffs and history
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional but nice icons
+    },
+    opts = {},
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>",       desc = "Diffview Open" },
+      { "<leader>gD", "<cmd>DiffviewClose<cr>",      desc = "Diffview Close" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History (current)" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>",  desc = "Diffview Repo History" },
+    },
+  },
 }
